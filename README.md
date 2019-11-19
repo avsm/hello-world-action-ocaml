e5ada891d6b8a06903bec295fd4a16b622e163bd
Anil Madhavapeddy <anil@recoil.org>
2019-11-19 17:28:24 +0000

    add hello world

---
+++ README.md
@@ -0,0 +1,6 @@
+# A Hello World OCaml Action
+
+This is a simple Hello World repository using OCaml and Dune.  It builds a
+library called `hello` and an executable called `world` and a test case that
+shows how to link in Alcotest.
+
---
+++ test.ml
@@ -0,0 +1,10 @@
+let test_hello () =
+  Alcotest.(check string) "same string" Hello.hello "hello world"
+
+let () =
+  let open Alcotest in
+  run "Hello" [
+      "World", [
+          test_case "Equal" `Quick test_hello;
+    ]
+  ]
