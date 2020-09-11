c5cb9694e51f30e86f27986b3a58a96984f713b0
liweijian <liweijian.hust@gmail.com>
2020-09-11 15:10:07 +0800

    update lib

--- test.ml
+++ test.ml
@@ -1,6 +1,6 @@
 let test_hello () =
-  Alcotest.(check string) "same string" Hello.hello "hello world!"
+  Alcotest.(check string) "same string" Hello.hello "hello world"
 
 let () =
   let open Alcotest in
-  run "Hello" [ ("World!", [ test_case "Equal" `Quick test_hello ]) ]
+  run "Hello" [ ("World", [ test_case "Equal" `Quick test_hello ]) ]
